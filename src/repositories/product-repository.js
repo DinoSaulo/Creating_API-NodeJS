38a2a73f86f5a7f458b430e1b0d9139d9d299d5b
Saulo Barros <sab2@cin.ufpe.br>
2019-07-24 23:59:59 -0300

    feat: inserido padrão 'Pattern Repository'

---
+++ src/repositories/product-repository.js
@@ -0,0 +1,61 @@
+// Repositório pattern
+// Esse repositório serve para evitar repetição e ajudar na reutilização de códigos pelos controllers
+// delega o acesso ao banco a esse repositório
+
+'use strict' // Forçando o javascript a ser estruturado
+
+const mongoose = require('mongoose');
+const Product = mongoose.model('Product');
+
+exports.get = () => {
+    return Product
+        .find({
+            active: true
+        }, 'title price slug');
+}
+
+exports.getBySlug = (slug) => {
+    return Product
+        .findOne({
+            slug: slug,
+            active: true
+        }, 'title description price slug tags');
+}
+
+exports.getById = (id) => {
+    return Product
+        .findById(id);
+}
+
+exports.getByTag = (tag) => {
+    return Product
+        .find({
+            tags: tag,
+            active: true
+        }, 'title description price slug tags');
+}
+
+// POST
+exports.create = (data) => {
+    var product = new Product(data);
+    return product.save();
+
+}
+
+// PUT
+exports.update = (id, data) => {
+    return Product
+        .findByIdAndUpdate(id, {
+            $set: {
+                title: data.title,
+                description: data.description,
+                price: data.price,
+                slug: data.slug
+            }
+        })
+}
+
+exports.delete = (id) => {
+    return Product
+        .findOneAndRemove(id);
+}
