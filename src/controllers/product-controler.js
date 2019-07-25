a4c4cd2c9da539dcc0c2ea521a07041d91454425
Saulo Barros <sab2@cin.ufpe.br>
2019-07-24 22:11:20 -0300

    feat: inserindo filtros

--- src/controllers/product-controler.js
+++ src/controllers/product-controler.js
@@ -1,8 +1,8 @@
 'use strict' // Forçando o javascript a ser estruturado
 
 const mongoose = require('mongoose');
-
 const Product = mongoose.model('Product');
+const ValidationContract = require('../validators/fluent-validator');
 
 exports.get = (req, res, next) => {
     Product
@@ -53,6 +53,18 @@ exports.getByTag = (req, res, next) => {
 };
 
 exports.post = (req, res, next) => {
+
+    let contract = new ValidationContract();
+    contract.hasMinLen(req.body.title, 3, 'O título deve conter pelo menos 3 caracteres');
+    contract.hasMinLen(req.body.slug, 3, 'O título deve conter pelo menos 3 caracteres');
+    contract.hasMinLen(req.body.description, 3, 'O título deve conter pelo menos 3 caracteres');
+
+    // Se os dados forem inválidos
+    if (!contract.isValid()) {
+        res.status(400).send(contract.errors()).end();
+        return;
+    }
+
     var product = new Product(req.body);
     product.save()
         .then(x => {
