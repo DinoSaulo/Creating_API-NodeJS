097777c0c7c7768c3407b7415aace992d4615290
Saulo Barros <sab2@cin.ufpe.br>
2019-07-27 15:09:24 -0300

    feat: envio de emails para cadastros

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo a loja!';
 
 module.exports = {
     connectionString: 'mongodb+srv://saulo:saulo@nodestr-swwcy.mongodb.net/test?retryWrites=true&w=majority',
-    sendgridKey: 'TBD',
+    sendgridKey: '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',
     containerConnectionString: 'TBD'
 }
