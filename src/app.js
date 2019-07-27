5270b38bfbc58664783515555a665bf66aee8e92
Saulo Barros <sab2@cin.ufpe.br>
2019-07-27 13:02:59 -0300

    refactor: criando dependencia para configurações

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const config = require('./config');
 
 const app = express();
 const router = express.Router(); // arquivos de rotas
 
 // conectando com o banco
-mongoose.connect('mongodb+srv://saulo:saulo@nodestr-swwcy.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect(config.connectionString);
 
 const Product = require('./models/product');
 const Customer = require('./models/customer');
