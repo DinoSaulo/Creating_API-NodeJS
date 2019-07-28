b0773dc7be70026d36d20377de245c9aaf60d26d
Saulo Barros <sab2@cin.ufpe.br>
2019-07-28 17:20:35 -0300

    refator: incluindo limitação de posts e habilitando o CORS

--- src/app.js
+++ src/app.js
@@ -20,8 +20,18 @@ const productRoute = require('./routes/product-route'); // carregando as rotas
 const customerRoute = require('./routes/customer-route'); // carregando as rotas
 const orderRoute = require('./routes/order-route'); // carregando as rotas
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Habilita o CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', indexRoute); // definindo a rota padrão
 app.use('/products', productRoute);
